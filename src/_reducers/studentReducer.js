81b4264775138e325c7c8c06f807fdc8eda982d7
Aksoni07 <ashishsoni7195@gmail.com>
2024-12-25 01:16:27 +0530

    some changes

--- src/_reducers/studentReducer.js
+++ src/_reducers/studentReducer.js
@@ -1,47 +1,43 @@
-import {CREATE_STUDENT, UPDATE_STUDENT, DELETE_STUDENTS} from './../_actionTypes/studentsActionTypes';
+import { CREATE_STUDENT, UPDATE_STUDENT, DELETE_STUDENTS } from './../_actionTypes/studentsActionTypes';
 
 let initialState = {
-    studentsArray : []
-}
-//an array element would be an object with student info
+        studentsArray: []
+    }
+    //an array element would be an object with student info
 
-const studenReducer = ( state = initialState, action) => {
+const studenReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case CREATE_STUDENT:
             return {
-                studentsArray : [...state.studentsArray, action.student]
+                studentsArray: [...state.studentsArray, action.student]
             }
 
         case UPDATE_STUDENT:
             let newStudentsArray = state.studentsArray.map((student, index) => {
-                if(student.email === action.updatedStudent.email) {
+                if (student.email === action.updatedStudent.email) {
                     return action.updatedStudent
                 }
                 return student;
             });
             return {
-                studentsArray : newStudentsArray
-            } 
+                studentsArray: newStudentsArray
+            }
 
         case DELETE_STUDENTS:
             let newArr = state.studentsArray.filter((student, index) => {
                 let exists = false;
-                exists = action.deletionArr.some((id) => {
-                    if(id === student.email) {
-                        return true;
-                    }
-                })
+                exists = action.deletionArr.some((id) => id === student.email)
                 return !exists;
             })
             return {
-                studentsArray : newArr
+                studentsArray: newArr
             }
 
         default:
             return state;
     }
-    
+
 }
 
 export default studenReducer;
